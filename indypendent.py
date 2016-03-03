fe11c32427cdba51de5a53b0746d8f9b97a60a0a
fiiidget <shackney@pratt.edu>
2016-03-03 11:29:24 -0500

    links
    what is this please remember

--- indypendent.py
+++ indypendent.py
@@ -3,6 +3,7 @@ import requests
 from time import sleep
 import sys
 import csv
+import urllib
 
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     enc = file.encoding
@@ -23,5 +24,17 @@ soup = BeautifulSoup(issue_html, "html.parser")
 
 article_box = soup.find_all("div", attrs = {"class": "ds-2col-stacked node node-issue node-promoted view-mode-full clearfix"})
 
+# for a_box in article_box:
+#     uprint(a_box.text)
+
 for a_box in article_box:
-    uprint(a_box.text])
+    article_links = a_box.find_all("div", attrs = {"class": "view-content"})
+
+    # for an_article in article_links:
+    #     uprint(an_article['href'])
+
+    for an_article in article_links:
+        a_link = an_article.find_all("a")
+        uprint(a_link["href"])
+
+        #
